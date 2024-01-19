3e80f4e59786dabaf1ed35b6bd88c6192cacf418
tomseimandi <tom.seimandi@gmail.com>
2024-01-19 17:41:36 +0000

    Add vector addition tutorial

---
+++ src/vector_addition.py
@@ -0,0 +1,108 @@
+"""
+Triton code for vector addition.
+"""
+import torch
+import triton
+import triton.language as tl
+import matplotlib
+
+
+@triton.jit
+def add_kernel(
+    X,
+    Y,
+    Z,
+    nel,
+    BLOCK_SIZE: tl.constexpr,  # NOTE: `constexpr` so it can be used as a shape value.
+):
+    """
+    Vector addition.
+
+    Args:
+        X: First vector pointer.
+        Y: Second vector pointer.
+        Z: Output pointer.
+        nel: Number of elements in vectors.
+        BLOCK_SIZE: Block size.
+    """
+    # 1D execution grid, so 0 index
+    pid = tl.program_id(axis=0)
+    # Each program sums the elements of the two vectors
+    # between pid * BLOCK_SIZE and (pid + 1) * NBLOCKS -1
+    offsets = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    # Mask
+    mask = offsets < nel
+    # Load
+    x = tl.load(X + offsets, mask=mask)
+    y = tl.load(Y + offsets, mask=mask)
+    # Sum
+    z = x + y
+    tl.store(Z + offsets, z, mask=mask)
+
+
+def add(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    """
+    Helper fn.
+
+    Args:
+        x (torch.Tensor): First tensor.
+        y (torch.Tensor): Second tensor.
+
+    Returns:
+        torch.Tensor: Addition result.
+    """
+    # Output pre-allocation.
+    output = torch.empty_like(x)
+    assert x.is_cuda and y.is_cuda and output.is_cuda
+    nel = output.numel()
+    # In this case, 1D grid where the size is the number of blocks:
+    grid = lambda meta: (triton.cdiv(nel, meta['BLOCK_SIZE']), )
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    add_kernel[grid](x, y, output, nel, BLOCK_SIZE=1024)
+    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+
+
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],  # Argument names to use as an x-axis for the plot.
+        x_vals=[2**i for i in range(12, 28, 1)],  # Different possible values for `x_name`.
+        x_log=True,  # x axis is logarithmic.
+        line_arg='provider',  # Argument name whose value corresponds to a different line in the plot.
+        line_vals=['triton', 'torch'],  # Possible values for `line_arg`.
+        line_names=['Triton', 'Torch'],  # Label name for the lines.
+        styles=[('blue', '-'), ('green', '-')],  # Line styles.
+        ylabel='GB/s',  # Label name for the y-axis.
+        plot_name='vector-add-performance',  # Name for the plot. Used also as a file name for saving the plot.
+        args={},  # Values for function arguments not in `x_names` and `y_name`.
+    ))
+def benchmark(size, provider):
+    x = torch.rand(size, device='cuda', dtype=torch.float32)
+    y = torch.rand(size, device='cuda', dtype=torch.float32)
+    quantiles = [0.5, 0.2, 0.8]
+    if provider == 'torch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x + y, quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add(x, y), quantiles=quantiles)
+    gbps = lambda ms: 12 * size / ms * 1e-6
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+    size = 98432
+    x = torch.rand(size, device='cuda')
+    y = torch.rand(size, device='cuda')
+    output_torch = x + y
+    output_triton = add(x, y)
+    print(output_torch)
+    print(output_triton)
+    print(f'The maximum difference between torch and triton is '
+          f'{torch.max(torch.abs(output_torch - output_triton))}')
+
+    # Benchmark
+    benchmark.run(print_data=True, show_plots=True)
